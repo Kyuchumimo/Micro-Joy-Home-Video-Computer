246977a978a59c22f81e19a9fa85c5d96f33f414
Kyuchumimo <74131798+Kyuchumimo@users.noreply.github.com>
2025-08-02 02:03:38 +0000

    Update wt588d_flash.py

--- tools/wt588d_flash.py
+++ tools/wt588d_flash.py
@@ -16,9 +16,9 @@ spi = machine.SPI(0,
                   phase=0,
                   bits=8,
                   firstbit=machine.SPI.MSB,
-                  sck=machine.Pin(2),
-                  mosi=machine.Pin(3),
-                  miso=machine.Pin(4))
+                  sck=Pin(2),
+                  mosi=Pin(3),
+                  miso=Pin(4))
 
 # Initialize SD card
 sd = sdcard.SDCard(spi, cs)
@@ -33,7 +33,7 @@ cspins = [Pin(13, Pin.OUT, value=1)]
 flash = FLASH(SPI(1, baudrate=20_000_000, sck=Pin(14), miso=Pin(12), mosi=Pin(11)), cspins)
 
 filename = "/sd/filename.winproj.bin"
-block_size = 1024
+block_size = 4096
 
 with open(filename, "rb") as f:
     start = 0  # Starting position for each block in flash
@@ -44,6 +44,8 @@ with open(filename, "rb") as f:
         
         # If there is no more data, exit the loop
         if data == b'':
+            print('Process has been successfully completed.')
+            
             break
 
         # Calculates the final position based on the actual size of `data`
@@ -51,9 +53,21 @@ with open(filename, "rb") as f:
 
         # Ensures that flash has the necessary size to write the block
         flash[start:end] = data
-        print(f"flash[{start}:{end}]: OK") if flash[start:end] == data else print(f"flash[{start}:{end}]: ERR")
+        
+        
+        if flash[start:end] == data:
+            print(f"flash[{start}:{end}]: OK")
+        else:
+            print(f"flash[{start}:{end}]: ERR")
+            print('An error has occurred during the data verification process. Process has been terminated.')
+            
+            break
 
         # Updates the starting position for the next block
         start = end
 
-print('OK')
+
+Pin(11, mode=Pin.IN, pull=None)
+Pin(12, mode=Pin.IN, pull=None)
+Pin(13, mode=Pin.IN, pull=None)
+Pin(14, mode=Pin.IN, pull=None)
